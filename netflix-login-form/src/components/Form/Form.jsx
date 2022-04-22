3ad5bcc1bb01c2725f69ae4e60ccd968877bdbd3
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-04-22 19:28:33 +0200

    form update

--- netflix-login-form/src/components/Form/Form.jsx
+++ netflix-login-form/src/components/Form/Form.jsx
@@ -1,32 +1,25 @@
-import "./styles.css"
+import "./styles.css";
 
 import { FormItem } from "../FormItem/FormItem";
 
 const formItems = [
   { cssClass: "email", type: "text", placeholder: "Email" },
   { cssClass: "password", type: "password", placeholder: "Password" },
-  { cssClass: "sign-in", type: "submit", href:"/#" },
+  { cssClass: "sign-in", type: "submit", href: "/#" },
 ];
 
 export const Form = () => {
   return (
     <form action="Sign-in">
-
-    {formItems.map((formItem, index) => (
-      <FormItem
-      
-        key={index}
-        cssClass={formItem.cssClass}
-        type={formItem.type}
-        placeholder={formItem.placeholder}
-        href={formItem.href}
-      />
-    ))}
-
-
-  </form>
-  )
-}
-
-
-
+      {formItems.map((formItem, index) => (
+        <FormItem
+          key={index}
+          cssClass={formItem.cssClass}
+          type={formItem.type}
+          placeholder={formItem.placeholder}
+          href={formItem.href}
+        />
+      ))}
+    </form>
+  );
+};
