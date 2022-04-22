dc00fe82ee1412238a2f3e022a98c333e2741dde
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-04-22 19:28:05 +0200

    footer list item component update

--- google-home-page/src/Components/footer/FooterListItem/FooterListItem.jsx
+++ google-home-page/src/Components/footer/FooterListItem/FooterListItem.jsx
@@ -1,12 +1,9 @@
 export const FooterListItem = (props) => {
-  console.log(props)
   return (
-    <>
-      {props.itemNames.map((itemName, index) => (
+      props.itemNames.map((itemName, index) => (
         <a className={props.itemClass} key={index} href="/#">
           <li>{itemName}</li>
         </a>
-      ))}
-    </>
+      ))
   );
 };
